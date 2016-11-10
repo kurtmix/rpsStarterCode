8b97017e4fe25f134ff97e5a4b19579963d26790
Tom Bijesse <tom@bijesse.com>
2016-11-10 15:02:48 -0500

    Update script.js

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,15 +1,15 @@
 // Task 1: Create a function that randomly chooses among 'rock', 'paper', or 'scissors'.
 var generateComputerChoice = function () {
     // Task 1, Step 1: Create an array with three elements ("rock", "paper" and "scissors").
-        var choices =["rock", "paper", "scissors"];
+        
     // Task 1, Step 2: Use the JavaScript Math function to generate a random whole
     // number between 0 and 2. Be sure to save it to a variable.
-        var randomPick = Math.floor(Math.random()*3);
+        
     // Task 1, Step 3: Use this randomly generated number to pull a value from the
     // array (eg myArray[randomNum])
-        var finalChoice = choices[randomPick];
+        
     // Task 1, Step 4: return this new value
-        return finalChoice;
+        
 };
 
 // Task 2: Create a function that compares the userChoice and the computerChoice
@@ -19,23 +19,12 @@ var pickWinner = function (userChoice, computerChoice) {
     console.log("computer choice: " + computerChoice);
     // Task 2, Step 1: Create an "if/else if/else" statement that compares the
     // userChoice and computerChoice under any possible game outcome.
-        if(userChoice === "rock" && computerChoice === "paper" || userChoice === "paper" && computerChoice === "scissors" || userChoice === "scissors" && computerChoice === "rock"){
-          console.log("Computer wins");
-          $("#winner").html("Computer Wins");
-        }
-        else if (userChoice === "rock" && computerChoice === "scissors" || userChoice === "paper" && computerChoice === "rock" || userChoice === "scissors" && computerChoice === "paper"){
-          console.log("User wins");
-          $("#winner").html("User Wins");
-        }
-        else{
-          console.log("Draw");
-          $("#winner").html("The game is a draw");
-        }
+       
     // Task 2, Step 2: Depending on who is the winner of the game console.log
     // either "user wins", "computer wins" or "draw"
 
     // Task 4: Show `computerChoice` in HTML after the words "Computer's choice:"
-    $("#computer-choice").html(computerChoice);
+    
     // Task 5: Show the winner in HTML after the word "Winner:"
 };
 
@@ -49,33 +38,16 @@ $(document).ready(function() {
 
     // This line sets `userChoice` variable to 'rock'. This value can be changed
     // manually when testing in the console.
-    var userChoice = null;
+    var userChoice = "rock";
     // Task 3: To be completed AFTER this game functions in the console.
-    // Set `userChoice` to "null" and create a click handler that changes the
-    // value based on the item the user clicks on the HTML page.
-    $("#rock").click(function(){
-      userChoice = "rock";
-      console.log(userChoice);
-      var computerChoice = generateComputerChoice();
-      pickWinner(userChoice, computerChoice);
-    });
-    $("#scissors").click(function(){
-      userChoice = "scissors";
-      console.log(userChoice);
-      var computerChoice = generateComputerChoice();
-      pickWinner(userChoice, computerChoice);
-    });
-    $("#paper").click(function(){
-      userChoice = "paper";
-      console.log(userChoice);
-      var computerChoice = generateComputerChoice();
-      pickWinner(userChoice, computerChoice);
-    });
+    // Set `userChoice` to "null" and create click handlers that changes the
+    // value of userChoice based on the item the user clicks on the HTML page.
+
     // This line calls the `pickWinner` function with the `userChoice` variable
     // and the `computerChoice` variable.
-
-    //Final Task: comment out the pickWinner function below and place it within your click functions created in Task 3.
+    pickWinner(userChoice, computerChoice);
+    //Final Task: comment out the pickWinner function on the line above and place it within your click functions created in Task 3.
     //this is done so the computer does not pick its throw until the user has also done so.
-    //pickWinner(userChoice, computerChoice);
+    
 
 });
