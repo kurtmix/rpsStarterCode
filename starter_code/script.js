3cdb19f59cd7a0e58a1df03a6fb00214991cdecf
kurtmix <kurtmix123@gmail.com>
2016-11-15 23:21:21 +0000

    First Step To Victory

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,17 +1,16 @@
 // Task 1: Create a function that randomly chooses among 'rock', 'paper', or 'scissors'.
 var generateComputerChoice = function () {
     // Task 1, Step 1: Create an array with three elements ("rock", "paper" and "scissors").
-        
+var computerChoice = ["rock","paper","scissors"];        
     // Task 1, Step 2: Use the JavaScript Math function to generate a random whole
     // number between 0 and 2. Be sure to save it to a variable.
-        
-    // Task 1, Step 3: Use this randomly generated number to pull a value from the
+var RandomRockPaperScissors = Math.floor(Math.random() * 3);
+  // Task 1, Step 3: Use this randomly generated number to pull a value from the
     // array (eg myArray[randomNum])
-        
+        var ComputerAns = computerChoice[RandomRockPaperScissors];
     // Task 1, Step 4: return this new value
-        
+        return ComputerAns;
 };
-
 // Task 2: Create a function that compares the userChoice and the computerChoice
 // to decide who won.
 var pickWinner = function (userChoice, computerChoice) {
